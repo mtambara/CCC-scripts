5caeff6a7a91bbaf37da0ccde0aeac87ce9eb339
Matthew Tambara <matthew.tambara@liferay.com>
2022-05-27 11:14:40 -0700

    Add more projects

--- LCHScripts/DevPriorities.js
+++ LCHScripts/DevPriorities.js
@@ -16,23 +16,28 @@ const spreadsheetId='1vrMv55hxrhfdP6Jwzj6P4II07iILAQhuRyudNbtnL30';
 const LCUTickets = "(project=LCU and (status in (open,ideas)))";
 const LCETickets = "(project=LCE and (status in (open,deferred)))";
 const LCDTickets = "(project=LCD and (status=open))";
+const LRSUPPORTTickets= "((project=LRSUPPORT) and (status=open))"
+const LRDOCSTickets= "((project=LRDOCS) and (status=open))"
+const SRETickets= "((project=SRE) and (status=open))"
+
+const queries = [LCUTickets, LCETickets, LCDTickets, LRSUPPORTTickets, LRDOCSTickets, SRETickets];
+
 const issueLink = "(issueLinkType != NULL)";
 
 async function main() {
 	const results = await fetchTickets();
-	let LCU = processTickets(results[0]);
-	let LCE = processTickets(results[1]);
-	let LCD = processTickets(results[2]);
 
-	writeTickets(LCU, LCE, LCD);
+	const tickets = [];
+
+	results.forEach(result => tickets.push(processTickets(result)));
+
+	writeTickets(tickets);
 }
 
 async function fetchTickets() {
 	const promises = [];
 
-	promises.push(jira.searchJira(`${LCUTickets} and ${issueLink}`, {maxResults:2000}));
-	promises.push(jira.searchJira(`${LCETickets} and ${issueLink}`, {maxResults:2000}));
-	promises.push(jira.searchJira(`${LCDTickets} and ${issueLink}`, {maxResults:2000}));
+	queries.forEach(query => promises.push(jira.searchJira(`${query} and ${issueLink}`, {maxResults:2000})));
 
 	return Promise.all(promises);
 }
@@ -67,7 +72,7 @@ function processTickets(results) {
 
 }
 
-async function writeTickets(...projects) {
+async function writeTickets(projects) {
 	const auth = new google.auth.GoogleAuth({
 		keyFile: `${process.env.GSHEETS_CREDENTIAL_PATH}`,
 		scopes: ['https://www.googleapis.com/auth/spreadsheets']
